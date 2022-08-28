b62bc5391c112bcf554f1cf9e2eee6208045c2fb
Nadezhda Komaritckaia <komaritskaya@gmail.com>
2022-08-28 12:37:39 -0500

    created brain-gcd pkg

---
+++ bin/brain-gcd.js
@@ -0,0 +1,3 @@
+import play from '../src/cli-gcd.js';
+
+play();
--- src/math.js
+++ src/math.js
@@ -17,3 +17,11 @@ export function getRandomOperator() {
 export function applyOperator(n1, n2, operator) {
   return operationsMap.get(operator)(n1, n2);
 }
+
+export function getGreatestCommonDivisor(n1, n2) {
+  if (!n2) {
+    return n1;
+  }
+
+  return getGreatestCommonDivisor(n2, n1 % n2);
+}
